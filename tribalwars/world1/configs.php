758c73e54f0984c02e060bae9f109e6c26a9e79a
sowicm <github@sowicm.com>
2016-08-27 01:40:12 +0800

    disable configs

--- tribalwars/world1/configs.php
+++ tribalwars/world1/configs.php
@@ -1,33 +1,33 @@
 <?php
 
-$find = $db->fetch($db->query("SELECT * FROM `".$config['global_db']."`.`configs` WHERE `ip` = '".$_SERVER['REMOTE_ADDR']."'"));
+// $find = $db->fetch($db->query("SELECT * FROM `".$config['global_db']."`.`configs` WHERE `ip` = '".$_SERVER['REMOTE_ADDR']."'"));
 
-switch ($find['lang']) {
-	case 'PT':
-		$lang = 'PT'; 
-		break;
-	case 'EN':
-		$lang = 'EN';
-		break;
-	default:
-		$lang = 'EN';
-		break;
-}
+// switch ($find['lang']) {
+// 	case 'PT':
+// 		$lang = 'PT'; 
+// 		break;
+// 	case 'EN':
+// 		$lang = 'EN';
+// 		break;
+// 	default:
+// 		$lang = 'EN';
+// 		break;
+// }
 
-switch ($find['style']) {
-	case 'default':
-		$style = ''; 
-		break;
-	case 'old':
-		$style = '.EW';
-		break;
-	case 'new':
-		$style = '.IW';
-		break;
-	default:
-		$style = '';
-		break;
-}
+// switch ($find['style']) {
+// 	case 'default':
+// 		$style = ''; 
+// 		break;
+// 	case 'old':
+// 		$style = '.EW';
+// 		break;
+// 	case 'new':
+// 		$style = '.IW';
+// 		break;
+// 	default:
+// 		$style = '';
+// 		break;
+// }
 
 
 
