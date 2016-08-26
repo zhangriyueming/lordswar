f05e1a23d4a4b3d7034dd2bd61743075a65cd685
sowicm <github@sowicm.com>
2016-08-26 20:56:21 +0800

    fix log

--- tribalwars/world1/daemons/logs.php
+++ tribalwars/world1/daemons/logs.php
@@ -7,10 +7,10 @@ define("PATH", str_replace(PATH_SEPARATOR, "/", dirname(dirname(__FILE__))));
 require_once(PATH."/include/config.php");
 require_once(PATH."/lib/DB_MySQL.php");
 $db = new DB_MySQL();
-$db->connect($config['db_dsn'], $config['db_user'], $config['db_pw']);
+$db->connect_($config['db_dsn'], $config['db_user'], $config['db_pw']);
 $test = $db->query("TRUNCATE `logs`");
 
-echo $test;
+echo $test->fetch();
 
 
 
