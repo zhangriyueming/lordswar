cccb0c689d0dc2964b3d54fe0021a342694de90b
sowicm <github@sowicm.com>
2016-08-30 20:15:20 +0800

    修复 rsync 命令

--- sync.sh
+++ sync.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 #
 
-rsync -h -v -P -t docker-compose_server.yml root@104.236.242.31:/root/docks/lordswar/docker-compose.yml
-rsync -h -v -P -t Dockerfile_server root@104.236.242.31:/root/docks/lordswar/Dockerfile
-rsync -h -v -P -t php5forum/Dockerfile_server root@104.236.242.31:/root/docks/lordswar/php5forum/Dockerfile
-rsync -h -v -P -t Dockerfile_server root@104.236.242.31:/root/docks/lordswar/Dockerfile
-rsync -h -v -P -t php-cron root@104.236.242.31:/root/docks/lordswar/php-cron
-rsync -h -v -r -P -t forum root@104.236.242.31:/root/docks/lordswar/forum
-rsync -h -v -r -P -t web root@104.236.242.31:/root/docks/lordswar/web
-rsync -h -v -r -P -t lordswar root@104.236.242.31:/root/docks/lordswar/lordswar
+rsync -vhtP docker-compose_server.yml root@104.236.242.31:/root/docks/lordswar/docker-compose.yml
+rsync -vhtP Dockerfile_server root@104.236.242.31:/root/docks/lordswar/Dockerfile
+rsync -vhtP php5forum/Dockerfile_server root@104.236.242.31:/root/docks/lordswar/php5forum/Dockerfile
+rsync -vhtP php-cron root@104.236.242.31:/root/docks/lordswar/php-cron
+rsync -avhtP forum/ root@104.236.242.31:/root/docks/lordswar/forum
+rsync -avhtP web/ root@104.236.242.31:/root/docks/lordswar/web
+rsync -avhtP lordswar/ root@104.236.242.31:/root/docks/lordswar/lordswar
 
