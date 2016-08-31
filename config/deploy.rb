b4eb285062a5c701ae8038536122e6702b5e5de0
sowicm <github@sowicm.com>
2016-08-31 23:35:10 +0800

    capistrano 部署

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+# 这样的话那何不全部放到一个 docker 里面？
+set :application, 'lordswar'
+repo_url = "https://#{ENV['LORDSWAR_GIT_USER']}:#{ENV['LORDSWAR_GIT_PASS']}@bitbucket.org/zhangriyueming/lordswar.git"
+set :repo_url, repo_url
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/root/docks/lordswar/cap'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
